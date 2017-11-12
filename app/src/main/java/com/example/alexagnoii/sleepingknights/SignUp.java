a85128ff607c5a5b1d54bdb7524e6833db740dc3
jesganoww <jesah_gano@dlsu.edu.ph>
2017-11-13 01:49:56 +0800

    changed UI

--- app/src/main/java/com/example/alexagnoii/sleepingknights/SignUp.java
+++ app/src/main/java/com/example/alexagnoii/sleepingknights/SignUp.java
@@ -1,11 +1,13 @@
 package com.example.alexagnoii.sleepingknights;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 /**
  * Created by jessganoww on 11/12/17.
@@ -15,6 +17,7 @@ public class SignUp extends AppCompatActivity {
 
     EditText username;
     Button btnsubmit;
+    RelativeLayout bg;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,6 +26,12 @@ public class SignUp extends AppCompatActivity {
 
         username = (EditText)findViewById(R.id.et_username);
         btnsubmit = (Button)findViewById(R.id.btn_submit);
+
+        bg = (RelativeLayout) findViewById(R.id.unamebg);
+        bg.setBackgroundResource(R.drawable.bgmove);
+
+        AnimationDrawable frAnim2 = (AnimationDrawable) bg.getBackground();
+        frAnim2.start();
 
         btnsubmit.setOnClickListener(new View.OnClickListener() {
             @Override
